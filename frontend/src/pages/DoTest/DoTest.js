295779bcaa1fde971879b6623cb5e7d16db92667
Ducminh-BB <creeper2k4@gmail.com>
2024-12-02 05:56:09 +0700

    fixed test

--- frontend/src/pages/DoTest/DoTest.js
+++ frontend/src/pages/DoTest/DoTest.js
@@ -23,7 +23,7 @@ const MAX_ATTEMPT = 5
 const CORRECT = 'Correct'
 const INCORRECT = 'Incorrect'
 const TIME_OUT = 'Timed Out'
-const SHUFFLE_VOCABS = 2 // indicates the vocabs need to reshuffle when their length is remaining X times. Min is 1
+const SHUFFLE_VOCABS = 5 // indicates the vocabs need to reshuffle when their length is remaining X times. Min is 5
 
 const getRandomVocab = (vocabs, test) => {
     const keys = Object.keys(vocabs) // get key of vocabs object
@@ -60,9 +60,13 @@ const DoTest = () => {
     const [maxTime, setMaxTime] = useState(TEST_MAX_TIME)
     const [ldError, setLdError] = useState(null)
     const [gainAttemptsTime, scaleGainAttemptsTime] = useState(1)
+    const [vocab, setVocab] = useState('')
     
     // track user leaving page
     const [testStarted, setTestStarted] = useState(false)
+
+    // track list of test detail
+    const [testDetailList, setTestDetailList] = useState([])
 
     // track animation of element
 
@@ -123,13 +127,15 @@ const DoTest = () => {
             console.error("can't fatch data")
         }
         
-
+        // eslint-disable-next-line
     }, [dispatch, ldDispatch])
 
     useEffect(() => {
-        if (vocabs)
+        if (vocabs || (duplVocabs && duplVocabs.length <= SHUFFLE_VOCABS))
             setDuplVocabs(vocabs)
-    }, [vocabs])
+
+        // eslint-disable-next-line
+    }, [vocabs, changeQuestionFlag])
 
     const pushDataToLd = async () => {
         // check the user existing in db
@@ -183,6 +189,20 @@ const DoTest = () => {
             }
     }
 
+    const updateTestDetails = () => {
+        if (testDetailList)
+        {
+            testDetailList.forEach(async (td) => {
+                await handleAddTestDetail(testDetail.current._id, {
+                    question: td.question,
+                    correctAns: td.correctAns,
+                    ans: td.ans,
+                    test: td.test
+                }, account.token)
+            })
+        }
+    }
+
     const renderTime = ({ remainingTime }) => {      
         return (
           <div className="timer">
@@ -207,6 +227,7 @@ const DoTest = () => {
             pushDataToLd()
         }
 
+        // eslint-disable-next-line
     }, [isDone])
 
     const refreshPage = () => {
@@ -229,6 +250,7 @@ const DoTest = () => {
     }
 
     const completeTest = () => {
+        updateTestDetails()
         handleAnimationEndingStart({setAnim: setIsAnimatedEndingScreen}, 4000)
         setTestStarted(false)
         setIsDone(true)
@@ -319,14 +341,19 @@ const DoTest = () => {
                 correct_ans = duplVocabs[index].english
                 checkAnswer(answer, correct_ans)
                 break
-       }             
-
-        await handleAddTestDetail(testDetail.current._id, {
+            default:
+                break
+       }
+       
+        const tsDetail = {
             question: vocabRef.current,
             correctAns: correct_ans,
             ans: answer,
             test: testDetail.current._id
-        }, account.token)
+        }
+        
+        // push tsDetail to list
+        setTestDetailList(oldTestDetail => [...oldTestDetail, tsDetail])
 
         if (index !== -1) {
             duplVocabs.splice(index, 1);
@@ -348,11 +375,6 @@ const DoTest = () => {
         e.preventDefault()
 
         await checkAnswerResult()
-
-        if (attempts === 0) {
-            completeTest()
-            return
-        }
 
     }
 
@@ -414,24 +436,19 @@ const DoTest = () => {
                 links.forEach((link) => link.removeEventListener("click", handleLinkClick))
             };
             ///////
-        }        
+        }
 
+        // eslint-disable-next-line
     }, [testStarted])
 
     useEffect(() => {
-        if (duplVocabs)
+        console.log(duplVocabs.length)
+        if (duplVocabs && test)
         {
-            if (duplVocabs.length === SHUFFLE_VOCABS)
-            {
-                setDuplVocabs(vocabs)
-            }
-            if (test)
-            {
-                let randVocab = getRandomVocab(duplVocabs, test)
-                vocabRef.current = randVocab
-            }
+            vocabRef.current = getRandomVocab(duplVocabs, test)
+            setVocab(vocabRef.current)
         }
-        
+        // eslint-disable-next-line
     }, [test, changeQuestionFlag])
 
     return (
@@ -550,7 +567,7 @@ const DoTest = () => {
                                                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><path fill="#cc2b1d" d="M1 11H31V21H1z"></path><path d="M5,4H27c2.208,0,4,1.792,4,4v4H1v-4c0-2.208,1.792-4,4-4Z"></path><path d="M5,20H27c2.208,0,4,1.792,4,4v4H1v-4c0-2.208,1.792-4,4-4Z" transform="rotate(180 16 24)" fill="#f8d147"></path><path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path></svg>
                                                         </div>
                                                         
-                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocabRef.current}</span>
+                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocab}</span>
                                                         
                                                     </div>
 
@@ -582,7 +599,7 @@ const DoTest = () => {
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><rect x="1" y="4" width="30" height="24" rx="4" ry="4" fill="#071b65"></rect><path d="M5.101,4h-.101c-1.981,0-3.615,1.444-3.933,3.334L26.899,28h.101c1.981,0,3.615-1.444,3.933-3.334L5.101,4Z" fill="#fff"></path><path d="M22.25,19h-2.5l9.934,7.947c.387-.353,.704-.777,.929-1.257l-8.363-6.691Z" fill="#b92932"></path><path d="M1.387,6.309l8.363,6.691h2.5L2.316,5.053c-.387,.353-.704,.777-.929,1.257Z" fill="#b92932"></path><path d="M5,28h.101L30.933,7.334c-.318-1.891-1.952-3.334-3.933-3.334h-.101L1.067,24.666c.318,1.891,1.952,3.334,3.933,3.334Z" fill="#fff"></path><rect x="13" y="4" width="6" height="24" fill="#fff"></rect><rect x="1" y="13" width="30" height="6" fill="#fff"></rect><rect x="14" y="4" width="4" height="24" fill="#b92932"></rect><rect x="14" y="1" width="4" height="30" transform="translate(32) rotate(90)" fill="#b92932"></rect><path d="M28.222,4.21l-9.222,7.376v1.414h.75l9.943-7.94c-.419-.384-.918-.671-1.471-.85Z" fill="#b92932"></path><path d="M2.328,26.957c.414,.374,.904,.656,1.447,.832l9.225-7.38v-1.408h-.75L2.328,26.957Z" fill="#b92932"></path><path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path></svg>
                                                         </div>
                                                         
-                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocabRef.current}</span>
+                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocab}</span>
                                                         
                                                     </div>
 
@@ -614,7 +631,7 @@ const DoTest = () => {
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32"><rect x="1" y="4" width="30" height="24" rx="4" ry="4" fill="#c93728"></rect><path d="M27,4H5c-2.209,0-4,1.791-4,4V24c0,2.209,1.791,4,4,4H27c2.209,0,4-1.791,4-4V8c0-2.209-1.791-4-4-4Zm3,20c0,1.654-1.346,3-3,3H5c-1.654,0-3-1.346-3-3V8c0-1.654,1.346-3,3-3H27c1.654,0,3,1.346,3,3V24Z" opacity=".15"></path><path d="M27,5H5c-1.657,0-3,1.343-3,3v1c0-1.657,1.343-3,3-3H27c1.657,0,3,1.343,3,3v-1c0-1.657-1.343-3-3-3Z" fill="#fff" opacity=".2"></path><path fill="#ff5" d="M18.008 16.366L21.257 14.006 17.241 14.006 16 10.186 14.759 14.006 10.743 14.006 13.992 16.366 12.751 20.186 16 17.825 19.249 20.186 18.008 16.366z"></path></svg>
                                                         </div>
                                                         
-                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocabRef.current}</span>
+                                                        <span className='position-absolute top-50 start-50 translate-middle fs-1' style={{userSelect:'none'}}>{vocab}</span>
                                                         
                                                     </div>
 
